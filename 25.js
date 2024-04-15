648acc5159f925d04ea3b040c1d51d5b809c2691
OlhaFedorets <fedoretsolha@gmail.com>
2024-04-15 11:39:05 +0300

    25 JS

---
+++ 25.js
@@ -0,0 +1,23 @@
+function goTown() {
+    update(locations[0]);
+  }
+  
+  function goStore() {
+  
+  }
+  
+  function goCave() {
+    console.log("Going to cave.");
+  }
+  
+  function fightDragon() {
+    console.log("Fighting dragon.");
+  }
+  
+  function buyHealth() {
+  
+  }
+  
+  function buyWeapon() {
+  
+  }
