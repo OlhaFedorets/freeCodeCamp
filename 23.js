cec08f5b0c98f9ff6906abf917f65652e10a0a14
OlhaFedorets <fedoretsolha@gmail.com>
2024-04-15 11:28:09 +0300

    24 JS

--- 23.js
+++
@@ -1,7 +0,0 @@
-const locations = [
-  {
-    name: "town square",
-    "button text": ["Go to store", "Go to cave", "Fight dragon"],
-    "button functions": [goStore, goCave, fightDragon]
-  }
-];
