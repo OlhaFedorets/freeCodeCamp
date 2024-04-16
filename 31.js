f0c31dd0527c93d4e49839d0e36c2c62eaed77c3
OlhaFedorets <fedoretsolha@gmail.com>
2024-04-16 16:58:27 +0300

    31 JS

--- 31.js
+++ 31.js
@@ -1,4 +1,5 @@
 function buyWeapon() {
+    if (currentWeapon < 3) {
     if (gold >= 30) {
       gold -= 30;
       currentWeapon++;
@@ -6,6 +7,9 @@ function buyWeapon() {
       let newWeapon = weapons[currentWeapon].name;
       text.innerText = "You now have a " + newWeapon + ".";
       inventory.push(newWeapon);
-      text.innerText += " In your inventory you have: "
+      text.innerText += " In your inventory you have: " + inventory;
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon.";
+    }
     }
   }
