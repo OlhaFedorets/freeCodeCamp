d6307118a27e75b8047c6b71e53a9fc526da6bc0
OlhaFedorets <fedoretsolha@gmail.com>
2024-04-16 16:00:56 +0300

    29 JS

--- 28.js
+++
@@ -1,6 +0,0 @@
-{
-    name: "cave",
-    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
-    "button functions": [fightSlime, fightBeast, goTown],
-    text: "You enter the cave. You see some monsters."
-  }
