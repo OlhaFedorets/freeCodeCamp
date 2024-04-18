e083a259ff8d9f5e8f4a4b319f6c3b2f1f43bd8e
OlhaFedorets <fedoretsolha@gmail.com>
2024-04-18 15:02:26 +0300

    32 JS

--- 32.js
+++ 32.js
@@ -1,7 +1,8 @@
 
 function sellWeapon() {
     if (inventory.length > 1) {
-        gold += 15;
-        goldText.innerText = gold
+      gold += 15;
+      goldText.innerText = gold;
+      let currentWeapon = inventory.shift();
     }
-}
+  }
