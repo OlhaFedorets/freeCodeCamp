a89073efaecc0c309e82f0e7a64cb01d6ef60d60
OlhaFedorets <fedoretsolha@gmail.com>
2024-04-18 15:25:33 +0300

    34 JS

--- 34.js
+++ 34.js
@@ -2,3 +2,13 @@ function fightSlime() {
     fighting = 0;
     goFight();
 }
+
+function fightBeast() {
+    fighting = 1;
+    goFight();
+}
+
+function fightDragon() {
+    fighting = 2;
+    goFight();
+}
