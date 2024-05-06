312d8f4b52ab2e7b7c13c647cad4ab10cd1d45eb
OlhaFedorets <fedoretsolha@gmail.com>
2024-05-06 16:49:29 +0300

    38.js

--- 38.js
+++ 38.js
@@ -1,8 +1,10 @@
 function dodge() {
     text.innerText = "You dodge the attack from the " + monsters[fighting].name
 }
-function defeatMonster() { 
+function defeatMonster() {
     gold += Math.floor(6.7 * monsters[fighting].level);
     xp += monsters[fighting].level;
- }
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+}
 function lose() { } 
