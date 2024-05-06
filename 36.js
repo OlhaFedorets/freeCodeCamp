bd6adb38a18c3c2ca6149e87830b3ff316741a11
OlhaFedorets <fedoretsolha@gmail.com>
2024-05-06 15:27:19 +0300

    36.js

--- 36.js
+++ 36.js
@@ -2,4 +2,6 @@ function goFight() {
     update(locations[3]);
     monsterHealth = monsters[fighting].health;
     monsterStats.style.display = 'block';
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealth
 }
