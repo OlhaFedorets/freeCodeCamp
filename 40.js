3bc52ef3473a99ec73d793c2105bc8498cf28119
OlhaFedorets <fedoretsolha@gmail.com>
2024-05-07 11:09:57 +0300

    40.js

--- 40.js
+++ 40.js
@@ -11,3 +11,14 @@ function update(location) {
 function lose() {
     update(locations[5])
 }
+function restart() {
+    xp = 0;
+    health = 100;
+    gold = 50;
+    currentWeapon = 0;
+    inventory = ["stick"];
+    goldText.innerText = gold;
+    healthText.innerText = health;
+    xpText.innerText = xp;
+    goTown()
+}
