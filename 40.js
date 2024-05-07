89d51510cd3db607548277dca33517b183a0a731
OlhaFedorets <fedoretsolha@gmail.com>
2024-05-07 12:20:35 +0300

    40.js

--- 40.js
+++ 40.js
@@ -18,7 +18,9 @@ function attack() {
 
 function getMonsterAttackValue(level) {
     const hit = (level * 5) - (Math.floor(Math.random() * xp));
-    } 
+    console.log(hit);
+    return hit;
+}
 
 function update(location) {
     monsterStats.style.display = "none";
