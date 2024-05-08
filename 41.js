e7d8957fe0bb491ea636501025b92f41e8726baf
OlhaFedorets <fedoretsolha@gmail.com>
2024-05-08 15:13:49 +0300

    41.js

--- 41.js
+++ 41.js
@@ -26,7 +26,7 @@ const locations = [
     {
         name: "kill monster",
         "button text": ["Go to town square", "Go to town square", "Go to town square"],
-        "button functions": [goTown, goTown, goTown],
+        "button functions": [goTown, goTown, easterEgg],
         text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
     },
     {
@@ -40,5 +40,11 @@ const locations = [
         "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
         "button functions": [restart, restart, restart],
         text: "You defeat the dragon! YOU WIN THE GAME! &#x1F389;"
-    }
+    },
+    { 
+        name: "easter egg", 
+        "button text": ["2", "8", "Go to town square?"], 
+        "button functions": [pickTwo, pickEight, goTown], 
+        text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!" 
+      }
 ]
